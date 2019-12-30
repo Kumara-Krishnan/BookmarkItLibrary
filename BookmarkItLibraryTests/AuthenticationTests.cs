7fcfd4801179034eb724b600fe087c28ebbf9b21
Kumara Krishnan <kumarakrishnanps@gmail.com>
2019-12-30 21:12:29 +0530

    Add unit tests

---
+++ BookmarkItLibraryTests/AuthenticationTests.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using BookmarkItLibrary.Data.Handler.Contract;
+using BookmarkItLibrary.DI;
+using BookmarkItLibrary.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.Security.Authentication.Web;
+
+namespace BookmarkItLibraryTests
+{
+    [TestClass]
+    public class AuthenticationTests
+    {
+        [TestMethod]
+        public async Task GetRequestTokenAsync()
+        {
+            var netHandler = BookmarkItLibrary.DI.DIServiceProvider.Instance.GetService<INetHandler>();
+            var requestToken = await netHandler.GetRequestTokenAsync();
+            Assert.IsTrue(requestToken.StartsWith("code="));
+        }
+
+
+    }
+}
